d2d91e52a0d064f3808858fbd30245b6a760c849
stepuro.yan <stepuro.yan@gmail.com>
2024-04-27 02:53:29 +0300

    first commit

---
+++ FinalWork_nursery/Main.java
@@ -0,0 +1,30 @@
+package org.FinalWork_nursery;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+
+        /*В программе должен быть реализован следующий функционал:
+        14.1 Завести новое животное
+        14.2 определять животное в правильный класс
+        14.3 увидеть список команд, которое выполняет животное
+        14.4 обучить животное новым командам
+        14.5 Реализовать навигацию по меню
+        15.Создайте класс Счетчик, у которого есть метод add(), увеличивающий̆
+        значение внутренней̆ int переменной̆ на 1 при нажатие “Завести новое
+        животное” Сделайте так, чтобы с объектом такого типа можно было работать в
+        блоке try-with-resources. Нужно бросить исключение, если работа с объектом
+        типа счетчик была не в ресурсном try и/или ресурс остался открыт. Значение
+        считать в ресурсе try, если при заведения животного заполнены все поля.*/
+
+
+        System.out.println("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
